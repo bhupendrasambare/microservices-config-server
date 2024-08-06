3f57a5dd5166fd661811fd13b2ce5bf01c948c7b
bhupendrasambare <bhupendrasam1404@gmail.com>
2024-08-07 01:50:52 +0530

    Auth and API gateway config added

--- src/main/java/com/service/config/ConfigServerApplication.java
+++ src/main/java/com/service/config/ConfigServerApplication.java
@@ -9,10 +9,12 @@ package com.service.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
